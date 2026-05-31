3f794128be58bb2304d6f6cb62d989ca7e6e8c9d
chaos-aa <taifaa@utas.edu.au>
2026-05-31 22:10:52 +1000

    Pathing fix 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://chaos-aa.github.io/halalblogtas/',
-  base: '/',
+  site: 'https://chaos-aa.github.io/',
+  base: '/halalblogtas',
 });
