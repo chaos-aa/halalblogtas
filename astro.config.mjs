2649f8f3895486d47b130f91bfb6afe6084c1aa4
chaos-aa <taifaa@utas.edu.au>
2026-05-31 22:06:44 +1000

    config fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://chaos-aa.github.io/halalblogtas/',
   base: '/',
 });
