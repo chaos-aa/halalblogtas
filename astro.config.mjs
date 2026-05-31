41533ef5d1ea505826907118c2d4154c0f4d0976
chaos-aa <taifaa@utas.edu.au>
2026-05-31 22:05:40 +1000

    Fresh start wiping the previous code clean and trying another format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://yourusername.github.io',
-  base: '/', // change to '/repo-name' if not using a custom domain
+  base: '/',
 });
